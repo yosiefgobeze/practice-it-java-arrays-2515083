dd8d5f8409182182cfd9966ba86f12cafa960b7c
yosiefgobeze <61394128+yosiefgobeze@users.noreply.github.com>
2023-10-11 07:10:35 +0000

    Iterating through an array

--- src/App.java
+++ src/App.java
@@ -11,8 +11,27 @@ public class App {
     lotteryNum[2] = 6; // initializing
     lotteryNum[3] = 8; // initializing
     lotteryNum[4] = 10; // initializing
+    System.out.println("Natural order print");
     for (int i = 0; i < lotteryNum.length; i++) {
       System.out.println(lotteryNum[i]);
     }
+
+    System.out.println("Reverse order print");
+    // printing in reverse order
+    for (int i = lotteryNum.length - 1; i >= 0; i--) {
+      System.out.println(lotteryNum[i]);
+    }
+
+    System.out.println("Natural order print, with enhanced for loop");
+    // enhanced for loop
+    for (double item : lotteryNum) {
+      System.out.println(item);
+    }
+
+    System.out.println("Using stream to print each item");
+    Arrays.stream(lotteryNum).forEach(System.out::println);
+    // Arrays.stream(lotteryNum).filter()
+    // Arrays.stream(lotteryNum).sum()
+    // Arrays.stream(lotteryNum).average()
   }
 }
