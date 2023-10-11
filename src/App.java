416e0ee3845f57aa5a20032bc11fff5da071d74e
yosiefgobeze <61394128+yosiefgobeze@users.noreply.github.com>
2023-10-11 06:41:23 +0000

    working with data

--- src/App.java
+++ src/App.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+
 public class App {
-  
+
   public static void main(String[] args) {
-    
+    // declare, define and initialize a double array.
+    double[] lotteryNum; // declaring
+    lotteryNum = new double[5]; // defining
+    lotteryNum[0] = 2; // initializing
+    lotteryNum[1] = 4; // initializing
+    lotteryNum[2] = 6; // initializing
+    lotteryNum[3] = 8; // initializing
+    lotteryNum[4] = 10; // initializing
+    for (int i = 0; i < lotteryNum.length; i++) {
+      System.out.println(lotteryNum[i]);
+    }
   }
 }
